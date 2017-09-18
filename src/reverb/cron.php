b7ee2236a7d4defa1a6c7aded8ddc8aac4905f0f
Johan Protin <jprotin@hipay.com>
2017-09-18 21:39:17 +0200

    fix cross origin

--- src/reverb/cron.php
+++ src/reverb/cron.php
@@ -8,8 +8,6 @@
  * @package Reverb
  */
 
-header("Access-Control-Allow-Origin: *");
-
 include(dirname(__FILE__) . '/../../config/config.inc.php');
 include(dirname(__FILE__) . '/../../init.php');
 require_once dirname(__FILE__) . '/classes/crons/OrdersSyncEngine.php';
